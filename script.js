b69560852729782426193273e14b3c316591701b
austimeh <48378328+austimeh@users.noreply.github.com>
2023-09-12 11:55:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// script.js
-function updateUTCTime() {
-  const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
-  const currentTime = new Date().toUTCString();
-  utcTimeElement.textContent = currentTime;
+// Function to update the current day of the week
+function updateDayOfTheWeek() {
+  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const currentDate = new Date();
+  const dayIndex = currentDate.getUTCDay();
+  const currentDay = daysOfWeek[dayIndex];
+  document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDay;
 }
 
-// Update UTC time on page load
-updateUTCTime();
+// Function to update the date and time in the format "Tuesday, September 12, 2023, 14:30:45 UTC"
+function updateFormattedDateTime() {
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+  const currentDate = new Date();
+  const formattedDateTime = currentDate.toLocaleDateString('en-US', options);
+  document.querySelector('[data-testid="currentUTCTime"]').textContent = formattedDateTime;
+}
 
-// Update UTC time every 1 minute
-setInterval(updateUTCTime, 60000);
+updateDayOfTheWeek();
+updateFormattedDateTime();
