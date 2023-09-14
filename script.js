c2a31bc5fe9a2f15f3bc0f3c114b2d9ce8b05767
austimeh <48378328+austimeh@users.noreply.github.com>
2023-09-14 15:58:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-// Function to update the current day of the week
-function updateDayOfTheWeek() {
-  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const currentDate = new Date();
-  const dayIndex = currentDate.getUTCDay();
-  const currentDay = daysOfWeek[dayIndex];
-  document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDay;
+// script.js
+function updateUTCTime() {
+  const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+  const currentTime = new Date().toUTCString().split(' ')[4]; // Extract time part
+  utcTimeElement.textContent = currentTime;
 }
 
-// Function to update the date and time in the format "Tuesday, September 12, 2023, 14:30:45 UTC"
-function updateFormattedDateTime() {
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
-  const currentDate = new Date();
-  const formattedDateTime = currentDate.toLocaleDateString('en-US', options);
-  document.querySelector('[data-testid="currentUTCTime"]').textContent = formattedDateTime;
-}
+updateUTCTime();
 
-updateDayOfTheWeek();
-updateFormattedDateTime();
+// Update UTC time every 1 second
+setInterval(updateUTCTime, 1000);
